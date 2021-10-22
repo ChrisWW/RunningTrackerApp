43789939fb661bb5c239733edac3ad5ce6d8afff
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-10-22 14:24:31 +0200

    Architecture upgrade, added

--- app/src/main/java/com/example/runningtrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/runningtrackerapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.runningtrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.example.runningtrackerapp.R
 import com.example.runningtrackerapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,6 +16,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}")
     }
 }
---
+++ app/src/main/java/com/example/runningtrackerapp/ui/fragments/SetupFragment.kt
@@ -0,0 +1,9 @@
+package com.example.runningtrackerapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.runningtrackerapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SetupFragment : Fragment(R.layout.fragment_setup) {
+}
